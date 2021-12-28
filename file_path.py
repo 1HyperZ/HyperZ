3e14a3c0d7ab1b1948a2125dfc5beff2a9d2f7ac
1HyperZ <omerzur06@gmail.com>
2021-12-28 20:46:41 +0200

    aviv

---
+++ file_path.py
@@ -0,0 +1,20 @@
+path = input("enter path: ")
+action = input("enter action(sort, rev or last: ")
+aviv = []
+with open(path, 'r') as file:
+    if action == "sort":
+        for line in file:
+            for word in line.split():
+                aviv.append(word)
+        print(sorted(aviv))
+    elif action == "rev":
+        for line in file:
+            print(line[::-1])
+    if action == "last":
+        num = input("enter number of last lines to read: ")
+        lines = file.readlines()
+        last_lines = lines[-int(num):]
+        print(last_lines)
+    file.close()
+
+
